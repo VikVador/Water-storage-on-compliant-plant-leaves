2e7d69e5b8257d86d7c3e025c51321c8d4926789
Arnaud <arnaud.remi@student.uliege.be>
2022-02-11 23:12:00 +0100

    j'écrase hein

--- Matlab/get_stiffness.m
+++ Matlab/get_stiffness.m
@@ -13,7 +13,7 @@ function [k] = get_stiffness(alpha, m, L, g)
 if nargin == 3
     g = 9.81;
 end
-k = m * L * g * cos(alpha) / (2 * alpha);
+k = abs(m * L * g * cos(alpha) / (2 * alpha));
 
 if ~isfinite(k)
     disp('Warning : infinite stiffness');
--- Matlab/leafModel.m
+++ Matlab/leafModel.m
@@ -16,27 +16,27 @@ clear all, close all
 %% Global parameters
 global k g m_0 L Q rho alpha_0
 g = 9.81;    % [m/s^2]
-L = 0.1;     % [m]
-m_0 = 0.001; % [kg]
-Q = 1e-5;    % [m^3/s]
+L = 0.07;     % [m]
+m_0 = 0.00025; % [kg]
+Q = 1e-6;    % [m^3/s]
 rho = 1000;  % [kg/m^3]
-alpha_0 = 0.1; % [rad] -----> TO BE CHANGED, CREATE A FUNCTION get_alpha0()
+alpha_0 = 0.01; % [rad] -----> TO BE CHANGED, CREATE A FUNCTION get_alpha0()
                %              with image processing
 k = get_stiffness(alpha_0, m_0, L);  % [N.m]
 
 %% Solving momentum equations
-t = 0:0.005:10; % [s]
+t = 0:0.01:60; % [s]
 alpha = zeros(size(t)); % [rad]
 m = m_0 + rho * Q .* t; % [kg]
 for i = 1 : length(t)
     F = @(x) [-k * x + m(i) * g * L * cos(x) / 2];
     options = optimoptions('fsolve','Display','none');
-    alpha(i) = fsolve(F, [alpha_0], options);
+    alpha(i) = fsolve(F, [0], options);
 end
 
 %% Plots
 figure;
-plot(t, alpha, 'k', 'Linewidth', 1.5);
+plot(t, abs(alpha), 'k', 'Linewidth', 1.5);
 grid on;
 xlabel('$t$ [s]', 'Fontsize', 14, 'Interpreter', 'latex');
 ylabel('$\alpha$ [rad]', 'Fontsize', 14, 'Interpreter', 'latex');
@@ -46,7 +46,7 @@ yticklabels({'0', '\pi/4', '\pi/2'});
 set(gca,'fontsize',16);
 
 figure;
-plot(m, alpha, 'k', 'Linewidth', 1.5);
+plot(m, abs(alpha), 'k', 'Linewidth', 1.5);
 grid on;
 xlabel('$m$ [kg]', 'Fontsize', 14, 'Interpreter', 'latex');
 ylabel('$\alpha$ [rad]', 'Fontsize', 14, 'Interpreter', 'latex');
