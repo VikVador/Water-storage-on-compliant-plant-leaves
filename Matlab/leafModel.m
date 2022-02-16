baef1eb0748fd128db291e83a67b5dd7ba5109a9
Arnaud <arnaud.remi@student.uliege.be>
2022-02-16 12:54:58 +0100

    Correction du fsolve qui ne convergait pas

--- Matlab/leafModel.m
+++ Matlab/leafModel.m
@@ -12,6 +12,7 @@
 %     sed with respect to the time.
 
 clear all, close all
+addpath('Functions/');
 
 %% Global parameters
 global k g m_0 L Q rho alpha_0
@@ -25,25 +26,15 @@ alpha_0 = 0.01; % [rad] -----> TO BE CHANGED, CREATE A FUNCTION get_alpha0()
 k = get_stiffness(alpha_0, m_0, L);  % [N.m]
 
 %% Solving momentum equations
-t = 0:0.01:60; % [s]
-alpha = zeros(size(t)); % [rad]
-m = m_0 + rho * Q .* t; % [kg]
-for i = 1 : length(t)
-    F = @(x) [-k * x + m(i) * g * L * cos(x) / 2];
+
+alpha = alpha_0:0.01:0.9*pi/2;
+for i = 1 : length(alpha)
+    F = @(x) [-k * alpha(i) + x * g * L * cos(alpha(i)) / 2];
     options = optimoptions('fsolve','Display','none');
-    alpha(i) = fsolve(F, [0], options);
+    m(i) = fsolve(F, [0], options);
 end
 
 %% Plots
-figure;
-plot(t, abs(alpha), 'k', 'Linewidth', 1.5);
-grid on;
-xlabel('$t$ [s]', 'Fontsize', 14, 'Interpreter', 'latex');
-ylabel('$\alpha$ [rad]', 'Fontsize', 14, 'Interpreter', 'latex');
-ylim([0 pi/2]);
-yticks([0 pi/4 pi/2]);
-yticklabels({'0', '\pi/4', '\pi/2'});
-set(gca,'fontsize',16);
 
 figure;
 plot(m, abs(alpha), 'k', 'Linewidth', 1.5);
